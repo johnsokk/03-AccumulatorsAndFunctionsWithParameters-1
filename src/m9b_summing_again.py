1a407428bc89c92b58edb5ffec65c5032371c97d
johnsokk <johnsokk@rose-hulman.edu>
2019-01-28 23:38:07 -0500

    Welcome to CSSE 120

--- src/m9b_summing_again.py
+++ src/m9b_summing_again.py
@@ -4,8 +4,8 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Kaia Johnson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -17,7 +17,7 @@ def main():
 def run_test_sum_powers():
     """ Tests the   sum_powers   function. """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_powers  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -29,6 +29,23 @@ def run_test_sum_powers():
     print('Testing the   sum_powers   function:')
     print('--------------------------------------------------')
 
+    # Test 1
+    expected = 3.80826
+    actual = sum_powers(5, -0.3)
+    print('Test 1 Expected', expected)
+    print('         Actual', actual)
+
+    # Test 2
+    expected = 144.45655
+    actual = sum_powers(100, 0.1)
+    print('Test 2 Expected', expected)
+    print('         Actual', actual)
+
+    # Test 3
+    expected = 12201
+    actual = sum_powers(6, 5)
+    print('Test 3 Expected', expected)
+    print('         Actual', actual)
 
 def sum_powers(n, p):
     """
@@ -42,8 +59,13 @@ def sum_powers(n, p):
       -- sum_powers(5, -0.3) returns about 3.80826
       -- sum_powers(100, 0.1) returns about 144.45655
     """
+    total = 0
+    for k in range(n):
+        total = total + (k+1)**p
+    return total
+
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers  to GENERATE
